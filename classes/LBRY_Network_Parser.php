bec1e200c6a4d8993f398c44aa4dd05a3635a7cf
Paul Kirby <paul@undergroundweblab.com>
2018-10-05 17:53:04 -0500

    Parsing of post content

--- classes/LBRY_Network_Parser.php
+++ classes/LBRY_Network_Parser.php
@@ -15,7 +15,26 @@ class LBRY_Network_Parser
 
     public function __construct()
     {
+        // COMBAK: Composer is not safe in a wordpress environment. May have to write our own package.
         require_once LBRY_ABSPATH . 'vendor/autoload.php';
-        $this->converter = new HtmlConverter();
+        $this->converter = new HtmlConverter(array(
+            'strip_tags' => true
+        ));
+        add_action('save_post', array($this, 'convert_to_markdown'));
+    }
+
+    public function convert_to_markdown($post_id)
+    {
+        $post = get_post($post_id);
+        $title =  '<h1>' . $post->post_title . '</h1>';
+
+        $featured_image = get_the_post_thumbnail($post);
+
+        $content = $title;
+        $content .= $featured_image . '<br />';
+        $content .= apply_filters('the_content', get_post($post_id)->post_content);
+        $converted = $this->converter->convert($content);
+
+        return $converted;
     }
 }
