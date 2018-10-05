2c70b95d281f3910e24120649d0576a307001a71
Paul Kirby <paul@undergroundweblab.com>
2018-10-05 16:50:42 -0500

    Added html-to-markdown package via composer

--- classes/LBRY_Network_Parser.php
+++ classes/LBRY_Network_Parser.php
@@ -1,16 +1,21 @@
 <?php
+use League\HTMLToMarkdown\HtmlConverter;
+
 /**
  * Parses wordpress posts to be ready for the LBRY Network
+ * Uses the Html-to-Markdown package
+ * https://github.com/thephpleague/html-to-markdown
  *
  * @package LBRYPress
  */
 
 class LBRY_Network_Parser
 {
-    /**
-     * [__construct description]
-     */
+    public $converter = null;
+
     public function __construct()
     {
+        require_once LBRY_ABSPATH . 'vendor/autoload.php';
+        $this->converter = new HtmlConverter();
     }
 }
