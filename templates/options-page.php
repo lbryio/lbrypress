03a86e63ac61d70fd592da70f42a3f42a4203584
Lem Smyth <lem@lemsmyth.com>
2022-02-13 19:02:57 -0600

    wallet total balance on hover

--- templates/options-page.php
+++ templates/options-page.php
@@ -8,13 +8,13 @@ defined('ABSPATH') || die(); // Exit if accessed directly
 $LBRY = LBRY();
 $wallet_balance = $LBRY->daemon->wallet_balance();
 $available_balance = $wallet_balance->result->available;
+$total_balance = $wallet_balance->result->total;
 $lbry_active_tab  = isset( $_GET['tab'] ) ? $_GET['tab'] : 'general';
-$channel_list = $LBRY->daemon->channel_list();
 ?>
 
 <div class="wrap">
     <h1><?php esc_html_e( get_admin_page_title(), 'lbrypress' ); ?></h1>
-    <h2 title="<?php echo esc_attr( number_format( $total_balance, 3, '.', ',' ) ); ?> Wallet Total Balance"><img src="<?php echo esc_url( plugin_dir_url( LBRY_PLUGIN_FILE ) . 'admin/images/lbc.png' ) ?>" class="icon icon-lbc wallet-icon-lbc"><code><?php esc_html_e( number_format( $available_balance, 3, '.', ',' ) ); ?></code> Wallet Available Balance</h2>
+    <h2><img src="<?php echo esc_url( plugin_dir_url( LBRY_PLUGIN_FILE ) . 'admin/images/lbc.png' ) ?>" class="icon icon-lbc wallet-icon-lbc" title="<?php echo esc_attr( number_format( $total_balance, 3, '.', ',' ) ); ?> Wallet Total Balance"><code title="<?php echo esc_attr( number_format( $total_balance, 3, '.', ',' ) ); ?> Wallet Total Balance"><?php esc_html_e( number_format( $available_balance, 3, '.', ',' ) ); ?></code> Wallet Available Balance</h2>
     <nav class="nav-tab-wrapper">
         <a href="<?php echo esc_url( admin_url( 'options.php?page=lbrypress&tab=general' ) ); ?>" class="nav-tab <?php echo $lbry_active_tab == 'general' || '' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Settings' ); ?></a>
         <a href="<?php echo esc_url( admin_url( 'admin.php?page=lbrypress&tab=channels' ) ); ?>" class="nav-tab <?php echo $lbry_active_tab == 'channels' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Channels' ); ?></a>
